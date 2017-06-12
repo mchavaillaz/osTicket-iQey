9b82c2266f3c285dbeae0be628131f82c6d0b26f
Matthieu <matthieu.chavaillaz@gmail.com>
2017-06-12 16:01:53 +0200

    refs #3384 - Remove context from kb-search.custom.inc.php + improve style of the search result list - Add new style in iqey.css used in the search result list

--- include/client/kb-search.custom.inc.php
+++ include/client/kb-search.custom.inc.php
@@ -1,7 +1,3 @@
-<?php
-// Get the current page context
-$pageContext = $myVaccinesUtilities->getCurrentContext($_GET['context']);
-?>
 <!-- Top bar section -->
 <?php
 $title = __('Frequently Asked Questions');
@@ -21,18 +17,15 @@ include CLIENTINC_DIR . 'search-in-faq.inc.php';
 			</h1>
             <?php
             if ($faqs->exists(true)) {
-                // Filter the FAQs according to the context
-                $faqsForContext = $myVaccinesUtilities->getFAQForContext($faqs, $pageContext);
                 echo '<div>'
                     . sprintf(
                         __('%d FAQs matched your search criteria.'),
                         count($faqsForContext))
                     . '<ol>';
-                foreach ($faqsForContext as $F) {
+                foreach ($faqs as $F) {
                     echo sprintf(
-                        '<li class="faq-search-result"><a href="faq.php?id=%d&context=%s" class="previewfaq">%s</a></li>',
+                        '<li class="faq-search-result"><a href="faq.php?id=%d" class="previewfaq">%s</a></li>',
                         $F->getId(),
-                        $pageContext,
                         $F->getLocalQuestion(),
                         $F->getVisibilityDescription());
                 }
