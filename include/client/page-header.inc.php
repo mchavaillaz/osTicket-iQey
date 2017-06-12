f2780c9efc9f76ea6b762ddfd05cf2a82812b449
Matthieu <matthieu@kuhodhcp-015.k.arpage.ch>
2017-06-12 12:04:52 +0200

    refs 3384 - Delete unused images - Rename image with correct name - Simplify the page-header.inc.php and the iqey.css

--- include/client/page-header.inc.php
+++ include/client/page-header.inc.php
@@ -1,6 +1,6 @@
-<div class="wrapper bg-<?php echo $pageContext?>">
+<div class="wrapper bg">
 	<div class="container">
-		<div class="top-bar <?php echo $pageContext; ?>">
+		<div class="top-bar">
 			<div class="page-title-content">
 				<h1><?php echo $title ?></h1>
 				<p><?php echo $text ?></p>
