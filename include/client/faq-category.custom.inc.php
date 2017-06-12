94014bc833420c1927a9a46016ba34d67c3368dc
Matthieu <matthieu.chavaillaz@gmail.com>
2017-06-12 13:59:12 +0200

    refs #3384 - Add new style for the fag category links in iqey.css - Use the new style in faq-category.custom.inc.php

--- include/client/faq-category.custom.inc.php
+++ include/client/faq-category.custom.inc.php
@@ -55,13 +55,13 @@ require(CLIENTINC_DIR . 'page-header.inc.php');
                 echo '<div id="faq"><ol>';
                 foreach ($faqs as $F) {
                     $attachments = $F->has_attachments ? '<span class="Icon file"></span>' : '';
-                    echo sprintf('<li><a href="faq.php?id=%d" style="color: black;">%s</a></li>',
+                    echo sprintf('<li class="faq-category-link"><a href="faq.php?id=%d" style="color: black;">%s</a></li>',
                         $F->getId(),
                         Format::htmlchars($F->getLocalQuestion()));
                 }
                 echo '</ol></div>';
             } else {
-                echo '<strong>' . __('This category does not have any FAQs.') . ' <a href="index.php">' . __('Back To Index') . '</a></strong>';
+                echo '<strong>' . __('This category does not have any FAQs.') . '<a href="index.php">' . __('Back To Index') . '</a></strong>';
             }
             ?>
 			<br>
